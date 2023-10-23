34819cc79d89929ec617602357acd1328544f39b
Jakob Truelsen <jakob@scalgo.com>
2023-10-23 16:44:53 +0200

    use qualified names when parsing drop expression
    Fix #9

--- src/lib.rs
+++ src/lib.rs
@@ -262,3 +262,17 @@ pub fn test_parse_alter_sql() {
     parse_statement(sql, &mut issues, &options);
     assert!(issues.is_empty(), "Issues: {:#?}", issues);
 }
+
+
+#[test]
+pub fn test_parse_delete_sql_with_schema() {
+    let sql = "DROP TABLE IF EXISTS `test_schema`.`test`";
+    let options = ParseOptions::new()
+        .dialect(SQLDialect::MariaDB)
+        .arguments(SQLArguments::QuestionMark)
+        .warn_unquoted_identifiers(false);
+
+    let mut issues = Vec::new();
+    parse_statement(sql, &mut issues, &options);
+    assert!(issues.is_empty(), "Issues: {:#?}", issues);
+}
